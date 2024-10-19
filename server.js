dcb289ae6689bb46cc3e2c4e3cce3e3bc13de99c
2joaojoao <joao.souza-silva@unesp.br>
2024-10-19 12:44:17 -0300

    resolvendo compativilidade bcrypt

--- server.js
+++ server.js
@@ -3,22 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const http = require('http');
-const { Server } = require("socket.io");
 
-// Importando rotas
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
+const tutorRoutes = require('./routes/tutors');
 const petRoutes = require('./routes/pets');
-const potentiometerRoutes = require('./routes/potentiometer'); // Nova rota
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Substitua pelo domínio do seu frontend
-    methods: ["GET", "POST"]
-  }
+app.get('/', (req, res) => {
+  res.send('Servidor Backend está funcionando!');
 });
 
 // Middlewares
@@ -29,8 +22,8 @@ app.use('/uploads', express.static('uploads')); // Serve arquivos estáticos
 // Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/tutors', tutorRoutes);
 app.use('/api/pets', petRoutes);
-app.use('/api/potentiometer', potentiometerRoutes); // Adiciona a nova rota
 
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -40,20 +33,9 @@ mongoose.connect(process.env.MONGODB_URI, {
   console.log('Conectado ao MongoDB');
   // Iniciar o servidor após a conexão bem-sucedida
   const PORT = process.env.PORT || 5000;
-  server.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
 }).catch((error) => {
   console.error('Erro ao conectar ao MongoDB:', error);
 });
-
-// Socket.io para comunicação em tempo real
-io.on('connection', (socket) => {
-  console.log('Um usuário conectado');
-  
-  socket.on('disconnect', () => {
-    console.log('Usuário desconectado');
-  });
-});
-
-module.exports = { io };
